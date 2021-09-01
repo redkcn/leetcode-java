19f8407c05e8aa64d495b7bcc822715d745e1dad
Zi Wen Yang <zi.wen.yang@ibm.com>
2021-09-01 14:31:18 +0800

    initial project

---
+++ src/main/java/io/github/redkcn/App.java
@@ -0,0 +1,13 @@
+package io.github.redkcn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
