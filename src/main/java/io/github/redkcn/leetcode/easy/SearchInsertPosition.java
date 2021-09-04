82d7f05d7e1d2047a6f24c8d0ff97862f9de0659
vicente <vicente@Matrix.local>
2021-09-05 01:27:02 +0800

    complete the coding and test case for 35 search insert position

--- src/main/java/io/github/redkcn/leetcode/easy/SearchInsertPosition.java
+++ src/main/java/io/github/redkcn/leetcode/easy/SearchInsertPosition.java
@@ -14,12 +14,27 @@ package io.github.redkcn.leetcode.easy;
 
 public class SearchInsertPosition {
 
+    /**
+     * use bisection method to search insert position
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public Integer searchInsertPosition(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
 
-        int middleIndex = nums.length % 2;
-
-
-        return null;
+        return left;
     }
 
 }
--- src/test/java/io/github/redkcn/leetcode/easy/SearchInsertPositionTest.java
+++ src/test/java/io/github/redkcn/leetcode/easy/SearchInsertPositionTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class SearchInsertPositionTest {
 
     @Test
-    public void testForSearch() {
+    public void testForNormalSearch() {
         int[] nums = {0, 3, 6, 8};
         int target = 6;
         SearchInsertPosition position = new SearchInsertPosition();
@@ -23,6 +23,33 @@ public class SearchInsertPositionTest {
     public void testForNotExists() {
         int[] nums = {0, 3, 6, 8};
         int target = 5;
+        SearchInsertPosition position = new SearchInsertPosition();
+        int index = position.searchInsertPosition(nums, target);
+        Assert.assertEquals(2, index);
+    }
+
+    @Test
+    public void testForLeft() {
+        int[] nums = {1, 3, 6, 8};
+        int target = 0;
+        SearchInsertPosition position = new SearchInsertPosition();
+        int index = position.searchInsertPosition(nums, target);
+        Assert.assertEquals(0, index);
+    }
+
+    @Test
+    public void testForRight() {
+        int[] nums = {1, 3, 6, 8};
+        int target = 9;
+        SearchInsertPosition position = new SearchInsertPosition();
+        int index = position.searchInsertPosition(nums, target);
+        Assert.assertEquals(4, index);
+    }
+
+    @Test
+    public void testForMiddle() {
+        int[] nums = {1, 3, 6, 8, 10};
+        int target = 6;
         SearchInsertPosition position = new SearchInsertPosition();
         int index = position.searchInsertPosition(nums, target);
         Assert.assertEquals(2, index);
